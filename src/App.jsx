22f5d4a6c784c0089273b87d091d7d227c92f912
Viktorqqu <2016alfa2016@gmail.com>
2025-05-07 18:05:05 +0500

    добавил мини-игру тест 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import WebApp from "@twa-dev/sdk";
 import "./App.css";
 
 function App() {
+  const [showMarket, setShowMarket] = useState(false);
   const [username, setUsername] = useState("...");
 
   useEffect(() => {
@@ -13,9 +14,27 @@ function App() {
   }, []);
 
   return (
-    <div className="game-container">
+    <div className="app-container">
       <h2 className="username">👤 @{username}</h2>
-      <div className="moving-box"></div>
+
+      {/* Двигающийся шар */}
+      <div className="circle"></div>
+
+      {/* Нижняя кнопка */}
+      <div className="bottom-menu">
+        <button className="market-button" onClick={() => setShowMarket(true)}>Маркет</button>
+      </div>
+
+      {/* Модальное окно */}
+      {showMarket && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>🛍️ Добро пожаловать в маркет!</h3>
+            <p>Здесь ты сможешь покупать усиления и массу 🐾</p>
+            <button onClick={() => setShowMarket(false)}>Назад</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
